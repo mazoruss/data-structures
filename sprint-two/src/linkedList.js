065fc81b7dbc8f5ed6363cd0716cfdf0d51c64e9
mazoruss <chesszhu@gmail.com>
2016-09-14 16:05:05 -0700

    Finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,20 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-      var addNode = Node(value);
+    var addNode = Node(value);
 
-      list.tail = addNode;
-
+    if (!list.head) {
+      list.head = addNode;
+      list.tail = list.head;
+    } else {
+      list.tail.next = addNode;
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
-      list.head = list.tail;
+    var result = list.head.value;
+    if (list.head === list.tail) {
+      list.head = null;
       list.tail = null;
+    } else if (list.head.next) {
+      list.head = list.head.next;
+    }
+    return result;
   };
 
   list.contains = function(target) {
-  };
+    var currentNode = list.head;
 
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
+  };
+  
   return list;
 };
 
